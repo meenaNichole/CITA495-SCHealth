bbf1173574991c49d15db8434dee5e2cbda9bd89
MeenaNRahimi <rahimimn@g.cofc.edu>
2020-04-21 23:43:14 -0400

    Added a save feature

--- js/comic.js
+++ js/comic.js
@@ -309,3 +309,32 @@ var width = window.innerWidth;
 
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------Save function------------------------------------------------------------------------
+
+document.getElementById('save').addEventListener('click', function() {
+        var pdf = new jsPDF('l', 'px', [stage.width(), stage.height()]);
+        pdf.setTextColor('#000000');
+        // first add texts
+        stage.find('Text').forEach(text => {
+          const size = text.fontSize() / 0.75; // convert pixels to points
+          pdf.setFontSize(size);
+          pdf.text(text.text(), text.x(), text.y(), {
+            baseline: 'top',
+            angle: -text.getAbsoluteRotation()
+          });
+        });
+
+        // then put image on top of texts (so texts are not visible)
+        pdf.addImage(
+          stage.toDataURL({ pixelRatio: 2 }),
+          0,
+          0,
+          stage.width(),
+          stage.height()
+        );
+
+        pdf.save('canvas.pdf');
+      });
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
